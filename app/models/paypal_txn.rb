e8864db6ae757892f4af215b5dd27be688b0ccab
Gabe Anzelini <me@gabeanzelini.com>
2011-01-12 21:48:40 -0800

    made PaypalTxn subclass ActiveRecord::Base as Transaction no longer exists in spree

--- app/models/paypal_txn.rb
+++ app/models/paypal_txn.rb
@@ -1,4 +1,4 @@
-class PaypalTxn < Transaction
+class PaypalTxn < ActiveRecord::Base
   enumerable_constant :txn_type, :constants => [:authorize, :capture, :purchase, :void, :credit, :denied, :unknown]
 
   def txn_type_name
